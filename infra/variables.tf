044ae76b5c42c2316ffa3371450e3843f4f3b870
Himanshumishra2 <161891307+Himanshumishra2@users.noreply.github.com>
2025-12-04 16:58:06 +0530

    Update variables.tf
    wrong syntax

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,6 +1,29 @@
-variable "location" { type = string, default = "eastus" }
-variable "prefix" { type = string, default = "demoaks" }
-variable "node_vm_size" { type = string, default = "Standard_D2s_v3" }
-variable "node_count" { type = number, default = 1 }
-variable "node_min_count" { type = number, default = 1 }
-variable "node_max_count" { type = number, default = 3 }
+variable "location" {
+  type    = string
+  default = "eastus"
+}
+
+variable "prefix" {
+  type    = string
+  default = "demoaks"
+}
+
+variable "node_vm_size" {
+  type    = string
+  default = "Standard_D2s_v3"
+}
+
+variable "node_count" {
+  type    = number
+  default = 1
+}
+
+variable "node_min_count" {
+  type    = number
+  default = 1
+}
+
+variable "node_max_count" {
+  type    = number
+  default = 3
+}
